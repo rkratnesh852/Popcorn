a8300c5c12d26c91665c262dad6a6ece5b0efd7b
ratnesh <kumarratnesh009@gmail.com>
2020-07-29 23:32:29 +0530

    checkout screen design

--- lib/screens/checkout.dart
+++ lib/screens/checkout.dart
@@ -1,29 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
 import '../widget/custom_edit_text.dart';
 
 class CheckOut extends StatelessWidget {
   Widget build(BuildContext context) {
-      return buildCheckOutUI(context);
+    return Container(
+      child: Column(
+        children: <Widget>[buildCouponUI(context), buildCheckOutUI(context)],
+      ),
+    );
+  }
+
+  Widget buildCouponUI(BuildContext context) {
+    return Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'Billing Details',
+            textAlign: TextAlign.center,
+            style:
+                TextStyle(color: Colors.white, backgroundColor: Colors.black),
+          ),
+        ],
+      ),
+    );
   }
 
   Widget buildCheckOutUI(BuildContext context) {
     return Column(
       children: <Widget>[
         CustomEditText('Full Name'),
-        Container(margin: EdgeInsets.only(top: 5.0,left: 5.0,right: 5.0),),
+        Container(
+          margin: EdgeInsets.only(top: 5.0, left: 5.0, right: 5.0),
+        ),
         CustomEditText('Address line two'),
-        Container(margin: EdgeInsets.only(top: 5.0,left: 5.0,right: 5.0),),
+        Container(
+          margin: EdgeInsets.only(top: 5.0, left: 5.0, right: 5.0),
+        ),
         CustomEditText('Town / City'),
-        Container(margin: EdgeInsets.only(top: 5.0,left: 5.0,right: 5.0),),
-        CustomEditText('E-Mail Address'),
-        /*Row(
+        Container(
+          margin: EdgeInsets.only(top: 5.0, left: 5.0, right: 5.0),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            CustomEditText('Full Name'),
-            CustomEditText('Full Name'),
+            Expanded(
+              child: CustomEditText('Post Code / ZIP'),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 5.0, left: 5.0, right: 5.0),
+            ),
+            Expanded(
+              child: CustomEditText('Phone'),
+            )
           ],
-        )*/
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 5.0, left: 5.0, right: 5.0),
+        ),
+        CustomEditText('E-Mail Address'),
         RaisedButton(
-          child: Text('NEXT-YOUR ORDER'),
+          color: Colors.green,
+          child: Text(
+            'NEXT-YOUR ORDER',
+            style: TextStyle(color: Colors.white),
+          ),
+          onPressed: () {},
         )
       ],
     );
